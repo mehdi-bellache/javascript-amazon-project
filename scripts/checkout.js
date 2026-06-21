1e3f07cfa6274028f62bf57f7fe84314a21c85c6
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-21 17:54:36 +0200

    add a deleteProductFromCart function in checkout.js file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,7 +10,7 @@ function buildProductHTML(cartProduct, product){
     html += `<div class="delivery-date">Delivery date: Tuesday, June 21</div>` ;
     html += `<div class="cart-item-details-grid"><img class="product-image" src=${product.image}>` ; 
     html += `<div class="cart-item-details"> <div class="product-name">${product.name}</div> <div class="product-price">$${formatCurrency(product.priceCents)} </div>` ;
-    html += `<div class="product-quantity"> <span> Quantity: <span class="quantity-label">${cartProduct.quantity}</span> </span> <span class="update-quantity-link link-primary"> Update </span> <span class="delete-quantity-link link-primary"> Delete </span> </div></div>`;
+    html += `<div class="product-quantity"> <span> Quantity: <span class="quantity-label">${cartProduct.quantity}</span> </span> <span class="update-quantity-link link-primary"> Update </span> <span class="delete-quantity-link link-primary js-delete-link"> Delete </span> </div></div>`;
     html += `<div class="delivery-options"> <div class="delivery-options-title"> Choose a delivery option: </div>` ;
     html += `<div class="delivery-option"> <input type="radio" checked class="delivery-option-input" name="delivery-option-${product.id}"> <div> <div class="delivery-option-date"> Tuesday, June 21 </div>`;
     html += `<div class="delivery-option-price"> FREE Shipping </div> </div> </div>`;
@@ -21,6 +21,24 @@ function buildProductHTML(cartProduct, product){
     
     return html ;
 }
+
+function deleteProductFromCart(cart, product){
+    cart.map(cartElement =>{ 
+        if( cartElement.productId === product.productId){
+            return false ;
+        }
+        else{
+            return true ;
+        }
+    })
+}
+
+document.querySelector('js-delete-link').addEventListener('click', () =>{
+    
+
+})
+// after clicking on delete button i should.
+// delete the product from the cart
 
 
 function buildAllProductsHTML(cart, products){
