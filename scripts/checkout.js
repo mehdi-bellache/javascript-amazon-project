9c71c9a765973ca1159405d8faef439dbdca5cc3
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-25 20:17:30 +0200

    feat: display calculated delivery dates for each option using dayjs

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,6 @@ import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js'; 
 
 
-
 function buildProductHTML(cartProduct, product){
 
     let html = `<div class = "cart-item-container js-cart-item-container-${product.id}">` ;
@@ -12,11 +11,11 @@ function buildProductHTML(cartProduct, product){
     html += `<div class="cart-item-details"> <div class="product-name">${product.name}</div> <div class="product-price">$${formatCurrency(product.priceCents)} </div>` ;
     html += `<div class="product-quantity"> <span> Quantity: <span class="quantity-label">${cartProduct.quantity}</span> </span> <span class="update-quantity-link link-primary"> Update </span> <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${product.id}"> Delete </span> </div></div>`;
     html += `<div class="delivery-options"> <div class="delivery-options-title"> Choose a delivery option: </div>` ;
-    html += `<div class="delivery-option"> <input type="radio" checked class="delivery-option-input" name="delivery-option-${product.id}"> <div> <div class="delivery-option-date"> Tuesday, June 21 </div>`;
+    html += `<div class="delivery-option"> <input type="radio" checked class="delivery-option-input" name="delivery-option-${product.id}"> <div> <div class="delivery-option-date delivery-option-date-js-0"></div>`;
     html += `<div class="delivery-option-price"> FREE Shipping </div> </div> </div>`;
-    html += `<div class="delivery-option"> <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}"> <div> <div class="delivery-option-date">Wednesday, June 15 </div>` ;
+    html += `<div class="delivery-option"> <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}"> <div> <div class="delivery-option-date delivery-option-date-js-1"></div>` ;
     html += `<div class="delivery-option-price"> $4.99 - Shipping </div> </div> </div> <div class="delivery-option"> <input type="radio" class="delivery-option-input" name="delivery-option-${product.id}">` ;
-    html += `<div> <div class="delivery-option-date"> Monday, June 13 </div> <div class="delivery-option-price"> $9.99 - Shipping </div>` ;
+    html += `<div> <div class="delivery-option-date delivery-option-date-js-2"></div> <div class="delivery-option-price"> $9.99 - Shipping </div>` ;
     html += `</div> </div> </div> </div> </div>`;
     
     return html ;
@@ -49,3 +48,19 @@ document.querySelectorAll('.js-delete-link').forEach(deleteButton => {deleteButt
         document.querySelector(`.js-cart-item-container-${productId}`).remove();
     })
 });
+
+
+const a = dayjs() ;
+const b = a.add(7, 'day');
+const c = a.add(3, 'day');
+const d = a.add(1, 'day');
+
+document.querySelectorAll('.delivery-option-date-js-0').forEach(deliveryOption =>{
+    deliveryOption.innerHTML = b.format('dddd, D MMMM');
+})
+document.querySelectorAll('.delivery-option-date-js-1').forEach(deliveryOption =>{
+    deliveryOption.innerHTML = c.format('dddd, D MMMM');
+})
+document.querySelectorAll('.delivery-option-date-js-2').forEach(deliveryOption =>{
+    deliveryOption.innerHTML = d.format('dddd, D MMMM');
+})
