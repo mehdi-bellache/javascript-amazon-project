b6288f82f5fefdb92ba22385a8e5bd93dc2f68da
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-27 22:03:50 +0200

    fix: dynamically update payment summary on item delete or delivery change

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,6 +3,7 @@ import {products} from '../../data/products.js';
 import {formatCurrency} from '.././utils/money.js'; 
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js' ;
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
+import {renderPaymentSummary} from './paymentSummary.js' ;
 
 
 
@@ -86,6 +87,7 @@ export function renderOrderSummary(){
             const productId = deleteButton.dataset.productId ;
             deleteProductFromCart(productId);
             document.querySelector(`.js-cart-item-container-${productId}`).remove();
+            renderPaymentSummary();
         })
     });
 
@@ -95,6 +97,7 @@ export function renderOrderSummary(){
             const {productId, deliveryOptionId} = element.dataset ;
             updateDeliveryOption(productId, deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
             
         })
 
