f3a32957e495a7eddef28bc36bfc6265427aacc9
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-27 16:03:56 +0200

    Add two imports in pamentSummary file

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
-import {countItems} from "./checkout/paymentSummary.js" ;
+import { renderPaymentSummary } from "./checkout/paymentSummary.js" ;
 
 renderOrderSummary();
-countItems();
+
+renderPaymentSummary();
--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -1,4 +1,7 @@
 import {cart} from '../../data/cart.js' ;
+import {products} from '../../data/products.js';
+import {formatCurrency} from '.././utils/money.js'; 
+
 
 
 
@@ -11,6 +14,19 @@ function countItems(){
     return itemsNumber ;
 
 }
-function renderProducts(){
-    document.querySelector('.js-items').innerHTML = `Items ${countItems()}:` ;
+
+// function countShipping(){
+//     let itemsNumber = 0 ;
+//     cart.forEach(element => {
+//         itemsNumber += element.quantity ; 
+//     });
+//     deliveryOptionId;
+
+//     return itemsNumber ;
+
+// }
+
+
+export function renderPaymentSummary(){
+    document.querySelector('.js-items').innerHTML = `Items (${countItems()}):` ;
 }
