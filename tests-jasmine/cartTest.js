97580574350724c4bc10dc195b0cd6df6a664844
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-28 14:14:08 +0200

    add cartTest file

---
+++ tests-jasmine/cartTest.js
@@ -0,0 +1,21 @@
+import { addToCart } from "../data/cart.js";
+
+
+
+// export function addToCart(productId){
+//     const matchingItem = cart.find(element => element.productId === productId);
+//     const quantity = Number(document.querySelector(`.js-quantity-selector-${productId}`).value);
+//     if(matchingItem){
+//         matchingItem.quantity ++ ;
+//     }
+//     else{
+//         cart.push({        
+//             productId,
+//             quantity,
+//             deliveryOptionId: '1'
+//         })
+//     }
+
+//     saveToStorage();
+
+// }
--- tests-jasmine/moneyTest.js
+++ tests-jasmine/moneyTest.js
@@ -8,7 +8,7 @@ describe('test suite: formatCurrency', () =>{
     it('works with 0', () =>{
         expect(formatCurrency(0)).toEqual('0.00');
     })
-    
+
     it('rounds up to the nearest cent', () =>{
         expect(formatCurrency(2000.5)).toEqual('20.01');
     })
