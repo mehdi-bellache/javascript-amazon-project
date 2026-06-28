d9065e96154d1ae5a8357ffae4b47468972c09f1
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-28 15:09:16 +0200

    add test code in cartTest file to test addToCart function

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,6 +1,19 @@
-import { addToCart } from "../../data/cart.js";
+import { cart, addToCart } from "../../data/cart.js";
 
+Describe('test suite: addToCart', () =>{
+    it('adds an existing product to the cart', () =>{
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
+        addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
+        expect(cart.length).toEqual(1)
 
+    })
+    it('adds a new product to the cart', () =>{
+        addToCart('3ebe75dc-64d2-4137-8860-1f5a963e534b')
+        expect(cart.length).toEqual(1);
+
+    })
+
+})
 
 // export function addToCart(productId){
 //     const matchingItem = cart.find(element => element.productId === productId);
