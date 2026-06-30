52d2eafed251e4319f2d02b4ea53c93f5e186632
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-30 22:39:36 +0200

    Add beforeEach to prevent code duplication

--- tests-jasmine/checkout/orderSummaryTest.js
+++ tests-jasmine/checkout/orderSummaryTest.js
@@ -3,37 +3,7 @@ import { cart, loadFromStorage } from "../../data/cart.js";
 
 
 describe('test suite: renderOrderSummary', () =>{
-    it('displays the cart', () => {
-        document.querySelector('.js-test-container').innerHTML =  `
-            <div class="js-order-summary"></div> ` ;
-
-        const productId1 = 'e4f64a65-1377-42bc-89a5-e572d19252e2' ;
-        const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d' ;
-            spyOn(localStorage, 'getItem').and.callFake(() =>{
-            return JSON.stringify([{
-                productId: productId1,
-                quantity: 2,
-                deliveryOptions: '2'
-
-            }, {
-                productId: productId2,
-                quantity: 1,
-                deliveryOptions: '2'
-            }]);
-        });
-        loadFromStorage();
-
-        renderOrderSummary();
-
-
-        expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2) ;
-        expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
-        expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
-
-        document.querySelector('.js-test-container').innerHTML =  `` ;
-    })
-
-    it('removes a product', () =>{
+    beforeEach(() =>{
         spyOn(localStorage, 'setItem');
 
         document.querySelector('.js-test-container').innerHTML =  `
@@ -57,6 +27,17 @@ describe('test suite: renderOrderSummary', () =>{
         loadFromStorage();
 
         renderOrderSummary();
+    })
+    it('displays the cart', () => {
+
+        expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(2) ;
+        expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
+        expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1');
+
+        document.querySelector('.js-test-container').innerHTML =  `` ;
+    })
+
+    it('removes a product', () =>{
 
         document.querySelector(`.js-delete-link-${productId1}`).click() ;
         expect(document.querySelectorAll('.js-cart-item-container').length).toEqual(1) ;
@@ -68,4 +49,4 @@ describe('test suite: renderOrderSummary', () =>{
         document.querySelector('.js-test-container').innerHTML =  `` ;
 
     })
-})
+})  
