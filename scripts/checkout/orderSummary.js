987d3632037cd551354f7d0082377c55183dd78e
mehdi-bellache <wassimbelll10@gmail.com>
2026-06-30 17:13:03 +0200

    Add import in orderSummary file and export the function renderPaymentSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,8 @@ import {products} from '../../data/products.js';
 import {formatCurrency} from '.././utils/money.js'; 
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js' ;
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-import {renderPaymentSummary } from './paymentSummary.js' ;
+import { renderPaymentSummary } from './paymentSummary.js' ;
+import { renderCheckoutHeader } from '../checkout.js' ;
 
 
 function deliveryOptionsHTML(cartProduct, product){
