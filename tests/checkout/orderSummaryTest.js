bb0b5bba63fadab00ce93cc0fd0b689ad50489dd
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-05 19:29:59 +0200

    import loadProductsFetch in orderSummaryTest file

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,13 +1,13 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { cart, loadFromStorage } from "../../data/cart.js";
-import { loadProducts } from "../../data/products.js";
+import { loadProducts, loadProductsFetch } from "../../data/products.js";
 
 
 describe('test suite: renderOrderSummary', () =>{
     const productId1 = 'e4f64a65-1377-42bc-89a5-e572d19252e2' ;
     const productId2 = '15b6fc6f-327a-4ec4-896f-486349e85a3d' ;
     beforeAll( (done) =>{
-        loadProducts(() =>{
+        loadProductsFetch().then(() =>{
             done();
         });
     })
