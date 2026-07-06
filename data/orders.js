ddee0a4245bc366f340ea4ca495af4d8911a8745
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-06 23:22:20 +0200

    add the function addOrder and the function saveToStorage in orders file

--- data/orders.js
+++ data/orders.js
@@ -1,1 +1,12 @@
 export const orders = [];
+
+
+function addOrder(order){
+    if(order !== null){
+        orders.unshift(order);
+    }
+}
+
+function saveToStorage(){
+    localStorage.setItem('orders', JSON.stringify(orders));
+}
