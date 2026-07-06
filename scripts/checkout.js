b419a11e736db8162577409781bf758de8c423b1
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-06 22:44:37 +0200

    Using throw and reject in loadPage function

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -10,10 +10,13 @@ import { loadCart } from "../data/cart.js";
 async function loadPage(){
 
     try{
+        // throw 'error1';
 
         await loadProductsFetch();
-        await new Promise((resolve) =>{
+        await new Promise((resolve, reject) =>{
+            // throw 'error2';
             loadCart(() =>{
+                // reject('error3');
                 resolve();
             });
         })
