90d4406686d2ef950b405cffbd136f618ea10617
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-08 21:02:40 +0200

    Add a new test suite updateDeliveryOption in cartTest file

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -62,11 +62,11 @@ describe('test suite: removeProductFromCart', () =>{
         spyOn(localStorage, 'getItem').and.callFake(() =>{
             return JSON.stringify([]);
         });
+
+        loadFromStorage();
     })
 
     it('remove a productId that is in the cart', () =>{
-        loadFromStorage();
-
         addToCart(productId1);
         removeProductFromCart(productId1);
         expect(cart.length).toEqual(0);
@@ -75,10 +75,12 @@ describe('test suite: removeProductFromCart', () =>{
         
     })
     it('remove a productId that is not in the cart', () =>{ 
-        loadFromStorage();
         addToCart(productId1, 1);
         removeProductFromCart('wrong-id');
         expect(cart.length).toEqual(1);
+        expect(cart[0].productId).toEqual('3ebe75dc-64d2-4137-8860-1f5a963e534b');
+        expect(cart[0].quantity).toEqual(1);
+        expect(cart[0].deliveryOptionId).toEqual('1');
         expect(localStorage.setItem).toHaveBeenCalledTimes(2);
         expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
             productId: productId1,
@@ -87,4 +89,25 @@ describe('test suite: removeProductFromCart', () =>{
         }]));
 
     })
+})
+
+describe('test suite: updateDeliveryOption', () =>{
+    const productId1 = '3ebe75dc-64d2-4137-8860-1f5a963e534b';
+    beforeEach(() =>{
+        spyOn(localStorage, 'setItem');
+        spyOn(localStorage, 'getItem').and.callFake(() =>{
+            return JSON.stringify([{
+                productId: productId1,
+                quantity: 1,
+                deliveryOptionId: '1'
+            }]);
+        });
+    })
+
+    it('update the delivery option', () =>{
+        loadFromStorage();
+    })
+
+
+
 })
