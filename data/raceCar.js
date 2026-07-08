398b1d763ad113bf81bdfeab883deeb0d783360d
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-08 13:52:17 +0200

    Add the property acceleration and ovveride the methods go, openTrund and closeTrunk

--- data/raceCar.js
+++ data/raceCar.js
@@ -1,7 +1,20 @@
 class RaceCar extends Car{
     acceleration;
-    constructor(){
-
+    constructor(acceleration){
+        super(brand, model);
+        this.acceleration = acceleration ;
     }
 
+    go(){
+        if( this.isTrunkOpen === false){
+            if( this.speed < 300){
+                this.speed += this.acceleration ;
+            }
+        }
+    }
+
+    openTrunk(){}
+    closeTrunk(){}
+
 }
+
