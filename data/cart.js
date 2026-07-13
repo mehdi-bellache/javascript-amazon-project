137db2ed32e4e143e40d2f6edaaa6c618af8cbea
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-13 21:23:21 +0200

    Add the function validDeliveryOption in deliveryOptions file

--- data/cart.js
+++ data/cart.js
@@ -65,7 +65,7 @@ export function updateDeliveryOption(productId, deliveryOptionId){
         }
         
     });
-    if(!matchingItem === undefined){
+    if(!matchingItem){
         return ;
     }
 
--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -39,3 +39,13 @@ export function calculateDeliveryDate(deliveryOption){
     }
     return deliveryDate.format('dddd, D MMMM');
 }
+
+export function validDeliveryOption(deliveryOptionId){
+    let found = false ;
+    deliveryOptions.forEach(option =>{
+        if(option.id === deliveryOptionId){
+            found = true ;
+        }
+    })
+    return found ;
+}
