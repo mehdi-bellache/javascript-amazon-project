322aa0ae726bf1148a374eef844492a7c5ae17c9
mehdi-bellache <wassimbelll10@gmail.com>
2026-07-15 12:48:09 +0200

    create a new test suite: Product in productsTest file

--- tests/data/productsTest.js
+++ tests/data/productsTest.js
@@ -1,6 +1,39 @@
 import { Product, Clothing, Appliance } from "../../data/products.js";
 
-describe('Product', () =>{
+
+ getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png` ;
+  }
+
+  getPrice(){
+    return formatCurrency(this.priceCents);
+  }
+
+  extraInfoHTML(){
+    return ``;
+  }
+
+describe('test suite: Product', () =>{
+    const product = new Product({
+        id: '1',
+        image: 'images/products/test.png',
+        rating: {
+            stars: 4.5,
+            count: 55
+        },
+        priceCents: 1899
+    })
+
+    it('', () =>{
+        expect(product.id).toEqual('1');
+        expect(product.image).toEqual('images/products/test.png');
+        expect(product.rating.stars).toEqual(4.5);
+        expect(product.rating.count).toEqual(55);
+        expect(product.priceCents).toEqual(1899);
+        expect(product.getStarsUrl()).toEqual(`images/ratings/rating-${product.rating.stars * 10}.png`);
+        expect(product.getPrice()).toEqual(18.99);
+        expect(product.extraInfoHTML()).toEqual(``);
+    })
 
 })
 
