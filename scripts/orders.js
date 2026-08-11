0a0db8e5a812a85dfc082b6c47f2e9b458098f81
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-11 21:19:52 +0200

    Add the function renderOrderHTML in orders file

--- scripts/orders.js
+++ scripts/orders.js
@@ -1,26 +1,91 @@
 import { orders } from "../data/orders.js";
+import { formatCurrency } from "./utils/money.js";
+import { Product, products } from "../data/products.js";
+import { cart } from "../data/cart-class.js";
+import {
+  deliveryOptions,
+  getDeliveryOption,
+  calculateDeliveryDate,
+} from "../data/deliveryOptions";
 
-{
-  /* <div class="order-container">
-          
-          <div class="order-header">
+// js - orders - grid;
+
+// fff(order)
+
+function fff(order) {
+  const hh = `<div class="order-header">
             <div class="order-header-left-section">
               <div class="order-date">
                 <div class="order-header-label">Order Placed:</div>
-                <div>August 12</div>
+                <div>${order.orderTime}</div>
               </div>
               <div class="order-total">
                 <div class="order-header-label">Total:</div>
-                <div>$35.06</div>
+                <div>$${formatCurrency(order.totalCostCents)}</div>
               </div>
             </div>
 
             <div class="order-header-right-section">
               <div class="order-header-label">Order ID:</div>
-              <div>27cba69d-4c3d-4098-b42d-ac7fa62b7664</div>
+              <div>${order.id}</</div>
             </div>
-          </div>
+          </div>`;
 
+  hh += gg(order.products);
+}
+
+function gg(orderProducts) {
+  const matchingProduct;
+  orderProducts.forEach((orderProduct) => {
+    matchingProduct = products.find(
+      (product) => product.id === orderProduct.id,
+    );
+  });
+  return ``;
+}
+
+function renderOrderItemHTML(orderProduct) {
+  const product = products.find((product) => product.id === orderProduct.id);
+  const cartItem = cart.cartItems.find(
+    (cartItem) => cartItem.id === orderProduct.id,
+  );
+
+  const deliveryOption = getDeliveryOption(cartItem.deliveryOptionId);
+
+  return `
+    <div class="product-image-container">
+      <img src="${product.image}">
+    </div>
+
+    <div class="product-details">
+      <div class="product-name">
+        ${product.name}
+      </div>
+      <div class="product-delivery-date">
+        Arriving on: ${calculateDeliveryDate(deliveryOption)}
+      </div>
+      <div class="product-quantity">
+        Quantity: ${cartItem.quantity}
+      </div>
+      <button class="buy-again-button button-primary">
+        <img class="buy-again-icon" src="images/icons/buy-again.png">
+        <span class="buy-again-message">Buy it again</span>
+      </button>
+    </div>
+
+    <div class="product-actions">
+      <a href="tracking.html?orderId=${orderProduct.id}&productId=${product.id}">
+        <button class="track-package-button button-secondary">
+          Track package
+        </button>
+      </a>
+    </div>
+  `;
+}
+
+function jjjj() {
+  return orders.forEach((order) => {
+    `<div class="order-container">
           <div class="order-details-grid">
             <div class="product-image-container">
               <img src="images/products/athletic-cotton-socks-6-pairs.jpg">
@@ -78,6 +143,6 @@ import { orders } from "../data/orders.js";
               </a>
             </div>
           </div>
-        </div>
- */
+        </div>`;
+  });
 }
