8e3a82b9b66689c9ee75e90c57982425de726051
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-13 14:14:01 +0200

    Add the function getProduct in products file

--- data/products.js
+++ data/products.js
@@ -1,5 +1,15 @@
 import { formatCurrency } from "../scripts/utils/money.js";
 
+export function getProduct(productId) {
+  let matchingProduct;
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
+  return matchingProduct;
+}
+
 export class Product {
   id;
   image;
