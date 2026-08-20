fe81312e32dc3de31644b353651d765cbf2c3c32
mehdi-bellache <wassimbelll10@gmail.com>
2026-08-20 20:32:05 +0200

    handle keydown event in search input

--- scripts/utils/search.js
+++ scripts/utils/search.js
@@ -1,13 +1,22 @@
+function performSearch() {
+  const searchInput = document.querySelector(".js-search-bar");
+  const searchResult = searchInput.value;
+  if (searchResult) {
+    window.location.href = `amazon.html?search=${searchResult}`;
+  } else {
+    window.location.href = `amazon.html`;
+  }
+}
+
 export function setupSearch() {
   const searchButton = document.querySelector(".js-search-button");
   const searchInput = document.querySelector(".js-search-bar");
 
-  searchButton.addEventListener("click", function () {
-    const searchResult = searchInput.value;
-    if (searchResult) {
-      window.location.href = `amazon.html?search=${searchResult}`;
-    } else {
-      window.location.href = `amazon.html`;
+  searchButton.addEventListener("click", performSearch);
+
+  searchInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      performSearch();
     }
   });
 }
